79a7db36849ab4de7409cdad11357004f72cd36e
ghdrudtj <ghdqwer432@gmail.com>
2024-12-05 10:00:35 +0900

    카드 뒤집기

---
+++ Assets/2_Script/04.DollMaker/Card.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer cardRenerer;
+    [SerializeField] private Sprite PartSprite;
+    [SerializeField] private Sprite backSprite;
+    private bool isFlipped = false;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void FilpCard()
+    {
+        Vector3 originalScale = transform.localScale;
+        Vector3 targetScale = new Vector3(0f, originalScale.y, originalScale.z);
+
+        transform.DOScale(targetScale, 0.2f).OnComplete(() =>
+        {
+            isFlipped = !isFlipped;
+            if (isFlipped)
+            {
+                cardRenerer.sprite = PartSprite;
+            }
+            else 
+            {
+                cardRenerer.sprite = backSprite;    
+            }
+            transform.DOScale(originalScale, 0.2f);
+        });
+
+    }
+    void OnMouseDown()
+    {
+        FilpCard();
+        Debug.Log("MouseDown");
+    }
+}
