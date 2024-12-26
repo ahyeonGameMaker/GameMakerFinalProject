1f536007f4874aa35ce4d6beab8b38f3edfb89bc
prokimssam <86967542+prokimssam@users.noreply.github.com>
2024-12-26 10:07:39 +0900

    인트로 화면 글, 버튼 수정

--- Assets/2_Script/00.Intro/CameraMovement.cs
+++ Assets/2_Script/00.Intro/CameraMovement.cs
@@ -30,13 +30,13 @@ public class CameraMovement : MonoBehaviour
 
 	public IEnumerator TypingText()
 	{
-		string[] strings = new string[]{ "함께 한 1년, 그리고 앞으로의 우리.",
-			"함께라서 더 즐거웠던 순간들",
+		string[] strings = new string[]{ 
+			"함께 한 1년, 그리고 앞으로의 우리.",
 			"매일이 새로운 도전과 배움",
-			"소소하지만 특별했던 일상",
 			"웃고 떠들던 교실이 그리울 거야",
 			"우리가 만든 게임, 우리가 만든 추억",
-			"게임으로 하나된 우리들의 이야기"};
+			"게임으로 하나된 우리들의 이야기를 미니게임으로 소개할께",
+			"화면을 클릭하여 게임을 시작하자!"};
 
 		TestTexts.text = "";
 		for (int t = 0; t < strings.Length; t++)
@@ -47,7 +47,6 @@ public class CameraMovement : MonoBehaviour
 				TestTexts.text = strings[t].Typing(i);
 				yield return new WaitForSeconds(0.03f);
 			}
-			// Wait 1 second per 1 sentence | 한 문장마다 1초씩 대기
 			yield return new WaitForSeconds(1f);
 		}
 	}
